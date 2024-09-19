28e20c489a341a4f37dbd9f22ec87b784c56aa6b
Ramil Rana <ramilrana20@gmail.com>
2024-09-19 16:55:10 +0800

    Chore: Practice with js up to the reverse string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = (word, number) => {
+  if (number < 0) {
+    return "ERROR"
+  } else {
+    let repeatedWord = ""
+    for (let i = 0; i < number; i++) {
+      repeatedWord += word // Concatenate the word directly
+    }
+    return repeatedWord
+  }
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
-};
+const reverseString = function (word) {
+  if (word.split("") > 1) {
+    word
+      .split(" ")
+      .reverse()
+      .map(eachWord.split("").reverse().join(""))
+      .join(" ")
+  } else {
+    return word.split("").reverse().join("")
+  }
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
