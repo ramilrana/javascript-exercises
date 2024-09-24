19cf717a9f9eb56ec0e0b85c4b51a01e49e52936
Ramil <ramilrana20@gmail.com>
2024-09-24 23:05:44 +0800

    leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,25 +2,16 @@ const leapYears = function (year) {
   if (year % 100 == 0) {
     if (year % 400 == 0) {
       return true
-    } else if (year % 4 == 0) {
+    } else {
+      return false
+    }
+  } else {
+    if (year % 4 == 0) {
       return true
     } else {
       return false
     }
-  } else if (year % 4 == 0) {
-    if (year % 400 == 0) {
-      if (year % 100 == 0) {
-        return true
-      } else {
-        return false
-      }
-    } else {
-      return true
-    }
-  } else {
-    return false
   }
 }
-
 // Do not edit below this line
 module.exports = leapYears
