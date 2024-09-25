2279e8da6c4d9c777c09dffa172333427bafe05f
Ramil Rana <ramilrana20@gmail.com>
2024-09-25 10:05:46 +0800

    Chore: Temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (value) {
+  let initValue = (value - 32) / 1.8
+  return Math.round(initValue * 10) / 10
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function (value) {
+  let initValue = value * 1.8 + 32
+  return Math.round(initValue * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
-};
+  convertToFahrenheit,
+}
